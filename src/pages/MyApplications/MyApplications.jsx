be5a3e4857ba34a2dbc45bb7fc810beca236f911
MThe001 <mdredwanulhaque01614591672@gmail.com>
2024-12-18 23:55:49 +0600

    fix:use_axios

--- src/pages/MyApplications/MyApplications.jsx
+++ src/pages/MyApplications/MyApplications.jsx
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import useAuth from '../../hooks/useAuth';
+import axios from 'axios';
 
 const MyApplications = () => {
     const { user } = useAuth();
     const [jobs, setJobs] = useState([]);
 
     useEffect(() => {
-        fetch(`http://localhost:5000/job-application?email=${user.email}`)
-            .then((response) => response.json())
-            .then((data) => setJobs(data));
+        // fetch(`http://localhost:5000/job-application?email=${user.email}`)
+        //     .then((response) => response.json())
+        //     .then((data) => setJobs(data));
+
+
+        axios.get(`http://localhost:5000/job-application?email=${user.email}`, {
+            withCredentials: true
+        })
+            .then(res => setJobs(res.data))
+
+
     }, [user.email]);
 
     return (
